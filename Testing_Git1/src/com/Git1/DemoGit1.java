be7c6abd30f1885fd0b037c89caeaf4fbfaf66ea
ANKUSH <ANKUSH@ANKUSH-PC>
2022-02-18 23:08:08 +0530

    By Payal

--- Testing_Git1/src/com/Git1/DemoGit1.java
+++ Testing_Git1/src/com/Git1/DemoGit1.java
@@ -2,6 +2,12 @@ package com.Git1;
 
 public class DemoGit1 
 {
+	
+	public void m1()
+	{
+		System.out.println("Hello M1");
+	}
+	
   public static void main(String[] args) 
   {
 	System.out.println("Hello");
